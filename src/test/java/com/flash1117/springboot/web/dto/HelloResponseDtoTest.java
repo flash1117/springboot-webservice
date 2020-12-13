a69e2b532500aef4d8e8444d4301ea6e6d419e32
김기태 <flash1117@naver.com>
2020-12-14 05:30:08 +0900

    make controller and testcode

---
+++ src/test/java/com/flash1117/springboot/web/dto/HelloResponseDtoTest.java
@@ -0,0 +1,24 @@
+package com.flash1117.springboot.web.dto;
+
+import org.junit.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
+
+public class HelloResponseDtoTest {
+
+    @Test
+    public void 롬복_기능_테스트() {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        // then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+
+
+}
