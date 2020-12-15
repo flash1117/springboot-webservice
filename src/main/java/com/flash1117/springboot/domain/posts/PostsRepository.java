2af040b52de067c1dfec8e8513b2ca28db4b3cba
김기태 <flash1117@naver.com>
2020-12-15 10:17:57 +0900

    add BaseTimeEntity

---
+++ src/main/java/com/flash1117/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.flash1117.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts,Long> {
+}
---
+++ src/main/java/com/flash1117/springboot/web/dto/PostsResponseDto.java
@@ -0,0 +1,21 @@
+package com.flash1117.springboot.web.dto;
+
+import com.flash1117.springboot.domain.posts.Posts;
+import com.flash1117.springboot.domain.posts.PostsRepository;
+import lombok.Getter;
+
+@Getter
+public class PostsResponseDto {
+
+    private Long id;
+    private String title;
+    private String content;
+    private String author;
+
+    public PostsResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.content = entity.getContent();
+        this.author = entity.getAuthor();
+    }
+}
