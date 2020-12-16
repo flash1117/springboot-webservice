958137d8b03facf44c0a3eb51938f8ecf48d590e
김기태 <flash1117@naver.com>
2020-12-16 16:59:09 +0900

    add search function

--- src/main/java/com/flash1117/springboot/service/PostsService.java
+++ src/main/java/com/flash1117/springboot/service/PostsService.java
@@ -2,12 +2,16 @@ package com.flash1117.springboot.service;
 
 import com.flash1117.springboot.domain.posts.Posts;
 import com.flash1117.springboot.domain.posts.PostsRepository;
+import com.flash1117.springboot.web.dto.PostsListResponseDto;
 import com.flash1117.springboot.web.dto.PostsResponseDto;
 import com.flash1117.springboot.web.dto.PostsSaveRequestDto;
 import com.flash1117.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -36,5 +40,12 @@ public class PostsService {
                 IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
