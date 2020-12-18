07dbb63fe52dfa65a2b9b2fd2d96b825ea541109
김기태 <flash1117@naver.com>
2020-12-18 12:40:08 +0900

    add social login Naver

--- src/main/java/com/flash1117/springboot/web/IndexController.java
+++ src/main/java/com/flash1117/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.flash1117.springboot.web;
 
+import com.flash1117.springboot.config.auth.LoginUser;
 import com.flash1117.springboot.config.auth.dto.SessionUser;
 import com.flash1117.springboot.service.PostsService;
 import com.flash1117.springboot.web.dto.PostsResponseDto;
@@ -17,12 +18,12 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
+//    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
--- src/test/java/com/flash1117/springboot/web/HelloControllerTest.java
+++ src/test/java/com/flash1117/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.flash1117.springboot.web;
 
+import com.flash1117.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -14,12 +18,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 @RunWith(SpringRunner.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+    excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE
+        , classes = SecurityConfig.class)
+    }
+)
 public class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
+    @WithMockUser(roles="USER")
     @Test
     public void hello가_리턴된다() throws Exception {
 
@@ -30,6 +40,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles="USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
