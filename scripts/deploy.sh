669dd1d6dc765eb09681786e4f3ffda2ca4ca9d0
김기태 <flash1117@naver.com>
2020-12-24 18:50:09 +0900

    config deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -14,6 +14,8 @@ CURRENT_PID=$(pgrep -fl mollangzzang-springboot-webservice | grep jar | awk '{pr
 echo "> 현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
 if [ -z "$CURRENT_PID" ]; then
+  echo "> 현재 구동중인 애플리케이션이 없으므로 종료하지 않습니다."
+else
   echo "> kill -15 $CURRENT_PID"
   kill -15 $CURRENT_PID
   sleep 5
@@ -28,6 +30,8 @@ echo "> JAR Name: $JAR_NAME"
 echo "> $JAR_NAME 에 실행권한 추가"
 
 chmod +x $JAR_NAME
+
+echo "> $JAR_NAME 실행"
 
 nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
