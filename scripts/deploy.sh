352bc8547fc081adbd90a23b12836123fe1bd7a2
김기태 <flash1117@naver.com>
2020-12-25 01:10:37 +0900

    revise deploy shell script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,7 +9,7 @@ cp $REPOSITORY/zip/*.jar $REPOSITORY/
 
 echo "> 현재 구동 중인 애플리케이션 pid 확인"
 
-CURRENT_PID=$(pgrep -fl mollangzzang-springboot-webservice | grep jar | awk '{print $1}')
+CURRENT_PID=$(pgrep -fl springboot-webservice | grep jar | awk '{print $1}')
 
 echo "> 현재 구동 중인 애플리케이션 pid : $CURRENT_PID"
 
@@ -36,4 +36,4 @@ echo "> $JAR_NAME 실행"
 nohup java -jar \
   -Dspring.config.location=classpath:/application.properties,classpath:/application-real.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
   -Dspring.profiles.active=real \
-  $JAR_NAME > $ REPOSITORY/nohup.out 2>&1 &
+  $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
